09096aaaff95f7ef90983ae8141e3b90a0465c48
Alexandre Orlandini <72550137+orionp99@users.noreply.github.com>
2021-01-20 12:21:40 +0100

    FIN creation login

--- laboutiqueinformatique/src/Form/RegisterType.php
+++ laboutiqueinformatique/src/Form/RegisterType.php
@@ -58,13 +58,15 @@ class RegisterType extends AbstractType
                 'invalid_message' => 'Le mot de passe et la confirmation doivent être identique.',
                 'label' => 'Votre mot de passe',
                 'required' => true,
-                'first_options' =>  ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Confirmez votre mot de passe'],
-                'attr' =>  [
-                    'placeholder' =>'Merci de saisir un mot de passe'
-                ]
+                'first_options' =>  [
+                    'label' => 'Mot de passe',
+                    'attr' =>  [
+                        'placeholder' =>'Merci de saisir un mot de passe.'
+                    ]     
+            ],
+            'second_options' =>[
+                'label' => 'Merci de confirmer votre mot de passe.']
             ])
-
 
             ->add('submit', SubmitType::class,[
                 'label'=> "Sinscrire"
