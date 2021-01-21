30b4cdb092bbaec906af6cb289da38b57de3c371
Alexandre Orlandini <72550137+orionp99@users.noreply.github.com>
2021-01-21 14:06:10 +0100

    3314 END SLUGWORK

--- laboutiqueinformatique/src/Controller/ProductController.php
+++ laboutiqueinformatique/src/Controller/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -16,16 +15,37 @@ class ProductController extends AbstractController
    {
        $this->entityManager = $entityManager;
    }
-   
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
-    {   
+
+
+     
+    public function index()
+    {
         $products = $this->entityManager->getRepository(Product::class)->findAll();
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * @Route("/produit/{slug}", name="product")
+     */
+
+    public function show($slug)
+    {   
+        dd($slug);
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug();
+
+        // if (!$product)
+        // {
+        //     return $this->redirectToRoute('products');
+        // }
+
+        return $this->render('product/show.html.twig', [
+        'products' => $products
         ]);
     }
 }
