e3671a348899a809e3bf401ec601fd11a67bfa1b
Alexandre Orlandini <72550137+orionp99@users.noreply.github.com>
2021-01-21 20:56:11 +0100

    fin création filtre search nav bar

--- laboutiqueinformatique/src/Controller/ProductController.php
+++ laboutiqueinformatique/src/Controller/ProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
@@ -19,12 +22,27 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */  
-    public function index()
+    public function index(Request $request)
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $products=$this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {    
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
@@ -32,7 +50,7 @@ class ProductController extends AbstractController
      * @Route("/produit/{slug}", name="product")
      */
     public function show($slug)
-    {   
+    {
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
 
         if (!$product){
