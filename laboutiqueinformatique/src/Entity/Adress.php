a71637615b22dcff6febcc6a471e3998e4dd9c05
Alexandre Orlandini <72550137+orionp99@users.noreply.github.com>
2021-01-23 20:17:58 +0100

    fin tunnel d'achat

--- laboutiqueinformatique/src/Entity/Adress.php
+++ laboutiqueinformatique/src/Entity/Adress.php
@@ -68,6 +68,11 @@ class Adress
      */
     private $phone;
 
+    public function __toString()
+    {
+        return $this->getName().'[br]'.$this->getAddress().'[br]'.$this.getCity().' - '.$this.getContry();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
